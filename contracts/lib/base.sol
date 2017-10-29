80f85a4d5dbeb0b81bfbe2da2daa1daa57146d97
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2017-10-29 19:16:15 +0100

    Initial commit.

---
+++ contracts/lib/base.sol
@@ -0,0 +1,64 @@
+/// base.sol -- basic ERC20 implementation
+
+// Copyright (C) 2015, 2016, 2017  DappHub, LLC
+
+// Licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License.
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND (express or implied).
+
+pragma solidity ^0.4.13;
+
+import "erc20/erc20.sol";
+import "ds-math/math.sol";
+
+contract DSTokenBase is ERC20, DSMath {
+    uint256                                            _supply;
+    mapping (address => uint256)                       _balances;
+    mapping (address => mapping (address => uint256))  _approvals;
+
+    function DSTokenBase(uint supply) public {
+        _balances[msg.sender] = supply;
+        _supply = supply;
+    }
+
+    function totalSupply() public view returns (uint) {
+        return _supply;
+    }
+    function balanceOf(address src) public view returns (uint) {
+        return _balances[src];
+    }
+    function allowance(address src, address guy) public view returns (uint) {
+        return _approvals[src][guy];
+    }
+
+    function transfer(address dst, uint wad) public returns (bool) {
+        return transferFrom(msg.sender, dst, wad);
+    }
+
+    function transferFrom(address src, address dst, uint wad)
+        public
+        returns (bool)
+    {
+        if (src != msg.sender) {
+            _approvals[src][msg.sender] = sub(_approvals[src][msg.sender], wad);
+        }
+
+        _balances[src] = sub(_balances[src], wad);
+        _balances[dst] = add(_balances[dst], wad);
+
+        Transfer(src, dst, wad);
+
+        return true;
+    }
+
+    function approve(address guy, uint wad) public returns (bool) {
+        _approvals[msg.sender][guy] = wad;
+
+        Approval(msg.sender, guy, wad);
+
+        return true;
+    }
+}
---
+++ contracts/lib/token.sol
@@ -0,0 +1,98 @@
+/// token.sol -- ERC20 implementation with minting and burning
+
+// Copyright (C) 2015, 2016, 2017  DappHub, LLC
+
+// Licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License.
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND (express or implied).
+
+pragma solidity ^0.4.13;
+
+import "ds-stop/stop.sol";
+
+import "./base.sol";
+
+contract DSToken is DSTokenBase(0), DSStop {
+
+    mapping (address => mapping (address => bool)) _trusted;
+
+    bytes32  public  symbol;
+    uint256  public  decimals = 18; // standard token precision. override to customize
+
+    function DSToken(bytes32 symbol_) public {
+        symbol = symbol_;
+    }
+
+    event Trust(address indexed src, address indexed guy, bool wat);
+    event Mint(address indexed guy, uint wad);
+    event Burn(address indexed guy, uint wad);
+
+    function trusted(address src, address guy) public view returns (bool) {
+        return _trusted[src][guy];
+    }
+    function trust(address guy, bool wat) public stoppable {
+        _trusted[msg.sender][guy] = wat;
+        Trust(msg.sender, guy, wat);
+    }
+
+    function approve(address guy, uint wad) public stoppable returns (bool) {
+        return super.approve(guy, wad);
+    }
+    function transferFrom(address src, address dst, uint wad)
+        public
+        stoppable
+        returns (bool)
+    {
+        if (src != msg.sender && !_trusted[src][msg.sender]) {
+            _approvals[src][msg.sender] = sub(_approvals[src][msg.sender], wad);
+        }
+
+        _balances[src] = sub(_balances[src], wad);
+        _balances[dst] = add(_balances[dst], wad);
+
+        Transfer(src, dst, wad);
+
+        return true;
+    }
+
+    function push(address dst, uint wad) public {
+        transferFrom(msg.sender, dst, wad);
+    }
+    function pull(address src, uint wad) public {
+        transferFrom(src, msg.sender, wad);
+    }
+    function move(address src, address dst, uint wad) public {
+        transferFrom(src, dst, wad);
+    }
+
+    function mint(uint wad) public {
+        mint(msg.sender, wad);
+    }
+    function burn(uint wad) public {
+        burn(msg.sender, wad);
+    }
+    function mint(address guy, uint wad) public auth stoppable {
+        _balances[guy] = add(_balances[guy], wad);
+        _supply = add(_supply, wad);
+        Mint(guy, wad);
+    }
+    function burn(address guy, uint wad) public auth stoppable {
+        if (guy != msg.sender && !_trusted[guy][msg.sender]) {
+            _approvals[guy][msg.sender] = sub(_approvals[guy][msg.sender], wad);
+        }
+
+        _balances[guy] = sub(_balances[guy], wad);
+        _supply = sub(_supply, wad);
+        Burn(guy, wad);
+    }
+
+    // Optional token name
+    bytes32   public  name = "";
+
+    function setName(bytes32 name_) public auth {
+        name = name_;
+    }
+}
