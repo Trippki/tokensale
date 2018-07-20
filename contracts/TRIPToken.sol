ada45eaa9d5aa4c820e28490f019cfaef5223877
jesuscript <konstantin.k.ed@gmail.com>
2018-07-20 12:20:16 +0100

    cleanup

--- contracts/TRIPToken.sol
+++ contracts/TRIPToken.sol
@@ -10,20 +10,20 @@ import "zeppelin-solidity/contracts/token/ERC20/PausableToken.sol";
  */
 
 contract TRIPToken is PausableToken, MintableToken {
-    string public constant name = "Trippki Token";
-    string public constant symbol = "TRIP";
-    uint8 public constant decimals = 18;
+	string public constant name = "Trippki Token";
+	string public constant symbol = "TRIP";
+	uint8 public constant decimals = 18;
 
-    event Burn(address indexed burner, uint256 value);
+	event Burn(address indexed burner, uint256 value);
 
-    /**
-     * @dev Burns a specific amount of tokens.
-     * @param _value The amount of token to be burned.
-     */
-    function burn(uint256 _value) external {
-        address burner = msg.sender;
-        balances[burner] = balances[burner].sub(_value);
-        totalSupply_ = totalSupply_.sub(_value);
-        Burn(burner, _value);
-    }
+	/**
+	 * @dev Burns a specific amount of tokens.
+	 * @param _value The amount of token to be burned.
+	 */
+	function burn(uint256 _value) external {
+		address burner = msg.sender;
+		balances[burner] = balances[burner].sub(_value);
+		totalSupply_ = totalSupply_.sub(_value);
+		emit Burn(burner, _value);
+	}
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,47 +1,5 @@
 const TRIPToken = artifacts.require("./TRIPToken.sol");
-const TRIPCrowdsale = artifacts.require("./TRIPCrowdsale.sol");
-const BigNumber = web3.BigNumber
-const dayInSecs = 86400
-
-const startTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 80
-const presaleEndTime = startTime + (86400 * 20) // 20 days
-const endTime = startTime + (dayInSecs * 60) // 60 days
-const rate = new BigNumber(500)
-const crowdsaleHardCapInWei = new BigNumber(36000e+18)
-const crowdsaleSoftCapInWei = new BigNumber(26000e+18)
-const preSaleCapInWei = new BigNumber(18000e+18)
 
 module.exports = function(deployer, network, [_, wallet, wallet2, wallet3]) {
-    if(network == 'rinkeby' || network == 'testnet') {
-        deployer.deploy(
-            TRIPCrowdsale,
-            startTime,
-            presaleEndTime,
-            endTime,
-            rate,
-            crowdsaleHardCapInWei,
-            crowdsaleSoftCapInWei,
-            preSaleCapInWei,
-            wallet,
-            wallet2,
-            wallet3
-        );
-    } else {
-        // token deployed only for testing purposes. NOTE: dont use it for the mainnet.
-        deployer.deploy(TRIPToken);
-
-        deployer.deploy(
-            TRIPCrowdsale,
-            startTime,
-            presaleEndTime,
-            endTime,
-            rate,
-            crowdsaleHardCapInWei,
-            crowdsaleSoftCapInWei,
-            preSaleCapInWei,
-            wallet,
-            wallet2,
-            wallet3
-        );
-    }
+  deployer.deploy(TRIPToken);
 };
