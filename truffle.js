ba7f93a7ab2725aadf5d99b35492e5293c687f7e
jesuscript <konstantin.k.ed@gmail.com>
2018-07-21 11:20:06 +0100

    remove cfg

--- truffle.js
+++ truffle.js
@@ -2,10 +2,5 @@ const HDWalletProvider = require("truffle-hdwallet-provider");
 
 module.exports = {
   networks: {
-    "ropsten-infura": {
-      provider: () => new HDWalletProvider("cousin flame train pumpkin version busy whisper evolve earth electric female tortoise", "https://ropsten.infura.io/EhXVIJjzrsBI3vtYfkj6"),
-      network_id: 3,
-      gas: 4700000
-    }
   }
 };
